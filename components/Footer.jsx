2f19f6e08a193113d512e7d3964e5b4db394d885
PurvikaJagtap <fs21co004.purvikajagtap@gmail.com>
2024-05-30 14:27:05 +0530

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react';
+
+const Footer = () => (
+    <Box textAlign='center' p='5' color='gray.600'>
+        Developed by Purvika Jagtap
+        <br />
+        &copy; 2023 &mdash; sanskritiSpaces.in
+    </Box>
+);
+
+export default Footer;
